6cd0ca9559c417beb2ff324c2dc89b9bb816f15b
Jonathan Butcher <footley@gmail.com>
2012-05-31 10:02:01 +0200

    directory restructuring

--- setup.py
+++ setup.py
@@ -9,45 +9,54 @@ def parse_requirements(file_name):
     etc.
     """
     requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line): # skip comments/empty lines
-            continue
-        else:
-            requirements.append(line)
+    with open(file_name) as _file:
+        for line in _file.readlines():
+            line = line.strip()
+            if re.match(r'(\s*#)|(\s*$)', line): # skip comments/empty lines
+                continue
+            else:
+                requirements.append(line)
     
     return requirements
+    
+long_desc = 'fork of http://code.google.com/p/python-epub-builder/ to programmatically build EPUB books.'
+with open(README.md) as _file:
+    long_desc = _file.read()
 
 setup(name='epubbuilder',
       version='1.0',
       description='fork of http://code.google.com/p/python-epub-builder/ to programmatically build EPUB books.',
+      long_description = long_desc,
       author='Jonathan Butcher',
       author_email='footley@gmail.com',
       url='https://github.com/footley/epubbuilder',
-      py_modules=['epubbuilder'],
       install_requires=parse_requirements('requirements.txt'),
-      data_files=[('templates', ['container.xml', 
-                                 'content.opf',
-                                 'image.html',
-                                 'title-page.html',
-                                 'toc.html',
-                                 'toc.ncx',
-                                 ]),
-                  ('test-files', ['revenge.500x800.jpg']),
-                  ('epubcheck-3.0b5', ['COPYING.txt',
-                                       'README.txt',
-                                       'epubcheck-3.0b5.jar',
-                                       'lib/batik-css-1.7.jar',
-                                       'lib/batik-license.txt',
-                                       'lib/batik-util-1.7.jar',
-                                       'lib/commons-compress-1.2.jar',
-                                       'lib/commons-compress-license.txt',
-                                       'lib/jing-20120227.jar',
-                                       'lib/jing-license.txt',
-                                       'lib/sac-1.3.jar',
-                                       'lib/sac-license.txt',
-                                       'lib/saxon-9.1.0.8.jar',
-                                       'lib/saxon-license.txt',
-                                       ]),
+      packages=['epubbuilder'],
+      package_data=[('epubbuilder/templates', [
+                         'container.xml', 
+                         'content.opf',
+                         'image.html',
+                         'title-page.html',
+                         'toc.html',
+                         'toc.ncx',
+                         ]),
+                    ('epubbuilder/test-files', ['revenge.500x800.jpg']),
+                    ('epubbuilder/epubcheck-3.0b5', [
+                           'COPYING.txt',
+                           'README.txt',
+                           'epubcheck-3.0b5.jar',
+                           'lib/batik-css-1.7.jar',
+                           'lib/batik-license.txt',
+                           'lib/batik-util-1.7.jar',
+                           'lib/commons-compress-1.2.jar',
+                           'lib/commons-compress-license.txt',
+                           'lib/jing-20120227.jar',
+                           'lib/jing-license.txt',
+                           'lib/sac-1.3.jar',
+                           'lib/sac-license.txt',
+                           'lib/saxon-9.1.0.8.jar',
+                           'lib/saxon-license.txt',
+                           ]),
                  ],
       )
 
