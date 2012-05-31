8145264bc2953433959fc328857eaac37212baea
Jonathan Butcher <footley@gmail.com>
2012-05-31 09:14:52 +0200

    add requirements.txt and parse it for setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from distutils.core import setup
+
+def parse_requirements(file_name):
+    """
+    Simple requirements parsing, only works for reqs of form:
+    genshi==0.6
+    genshi
+    etc.
+    """
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line): # skip comments/empty lines
+            continue
+        else:
+            requirements.append(line)
+    
+    return requirements
+
 setup(name='epubbuilder',
       version='1.0',
       description='fork of http://code.google.com/p/python-epub-builder/ to programmatically build EPUB books.',
@@ -6,6 +23,7 @@ setup(name='epubbuilder',
       author_email='footley@gmail.com',
       url='https://github.com/footley/epubbuilder',
       py_modules=['epubbuilder'],
+      install_requires=parse_requirements('requirements.txt'),
       data_files=[('templates', ['container.xml', 
                                  'content.opf',
                                  'image.html',
